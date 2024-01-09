da5b553d5b7b63b532ee841f3c3fd34d948ac27d
rahdy <rahdy@code-rhapsodie.fr>
2024-01-09 17:03:29 +0100

    add job

--- src/Controller/GitCloneController.php
+++ src/Controller/GitCloneController.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Controller;
+use App\Entity\Job;
+use App\Entity\Rapport;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,7 +26,7 @@ class GitCloneController extends AbstractController
 	}
 
 	#[Route('/git-clone', name: 'git_clone')]
-	public function gitClone(Request $request): Response
+	public function gitClone(Request $request, EntityManagerInterface $entityManager): Response
 	{
 		// Récupérer l'URL du dépôt Git depuis la requête
 		$repositoryUrl = $request->query->get('repository');
@@ -33,37 +36,90 @@ class GitCloneController extends AbstractController
 			return new Response('Aucune URL de dépôt spécifiée.', Response::HTTP_BAD_REQUEST);
 		}
 
-		// Validation de l'URL Git
-		/*if (!$this->isValidGitUrl($repositoryUrl)) {
-			return new Response('L\'URL du dépôt n\'est pas valide.', Response::HTTP_BAD_REQUEST);
-		}*/
+		// Chemin relatif du répertoire de destination
+		$destination = 'repoClone'; // Utiliser un chemin relatif par rapport à la racine du projet
 
-		// Construction de la commande git clone
-		$command = "git clone $repositoryUrl";
-
-		// Créer un processus pour exécuter la commande
-		$process = Process::fromShellCommandline($command);
+		// Créer un processus pour exécuter la commande git clone dans le répertoire spécifié
+		$process = new Process(["git", "clone", $repositoryUrl, $destination]);
 
 		try {
 			// Exécuter la commande git clone
 			$process->mustRun();
 
-			// Récupérer la sortie de la commande
-			$output = $process->getOutput();
+			// Exécuter et enregistrer la commande Composer Audit
+			$composerAuditOutput = $this->executeComposerAudit($destination);
+			$php_vOutput = $this->executePHP_V($destination);
+
+			// Préparer le détail pour le stockage
+			$detailComposerAudit = empty($composerAuditOutput) ? ['result' => 'Aucune faille'] : ['result' => $composerAuditOutput];
+			$detailPhp_v = empty($php_vOutput) ? ['result' => 'Aucune faille'] : ['result' => $php_vOutput];
+
+			// Enregistrer le résultat de Composer Audit en tant que job
+			$composerAuditJob = new Job();
+			$composerAuditJob->setName('Composer Audit');
+			$composerAuditJob->setResultat(empty($composerAuditOutput) ? 'Aucune faille' : $composerAuditOutput);
+			$composerAuditJob->setDetail($detailComposerAudit); // Assigner le détail sous forme de tableau
+			$entityManager->persist($composerAuditJob);
+			$entityManager->flush();
+
+			$Php_vJob = new Job();
+			$Php_vJob->setName('Php Version');
+			$Php_vJob->setResultat(empty($php_vOutput) ? 'Aucune faille' : $php_vOutput);
+			$Php_vJob->setDetail($detailPhp_v); // Assigner le détail sous forme de tableau
+			$entityManager->persist($Php_vJob);
+			$entityManager->flush();
+
+
+			//$raport = new Rapport();
 
 			// Renvoyer la sortie comme réponse
-			 new Response($output);
-
-			 $message ="";
-			return $this->render('project/show.html.twig', [
+			$message = "Clonage du dépôt Git réussi.";
+			return $this->render('git_clone/resultat.html.twig', [
 				'message' => $message,
 			]);
 		} catch (ProcessFailedException $exception) {
-			// En cas d'échec, récupérer et renvoyer l'erreur
+			// En cas d'échec du clonage ou de Composer Audit, récupérer et renvoyer l'erreur
 			return new Response($exception->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
 		}
 	}
 
+
+	private function executeComposerAudit(): string
+	{
+		$composerAuditCommand = 'composer audit --locked --format=json';
+
+		// Créer un processus pour exécuter la commande Composer Audit dans le répertoire cloné
+		$process = Process::fromShellCommandline($composerAuditCommand);
+
+		try {
+			// Exécuter la commande Composer Audit
+			$process->mustRun();
+
+			// Récupérer la sortie de la commande
+			return $process->getOutput();
+		} catch (ProcessFailedException $exception) {
+			// En cas d'échec de Composer Audit, retourner un message d'erreur
+			return $exception->getMessage();
+		}
+	}
+	private function executePHP_V(): string
+	{
+		$php_vCommand = 'php-v';
+
+		// Créer un processus pour exécuter la commande Composer Audit dans le répertoire cloné
+		$process = Process::fromShellCommandline($php_vCommand);
+
+		try {
+			// Exécuter la commande Composer Audit
+			$process->mustRun();
+
+			// Récupérer la sortie de la commande
+			return $process->getOutput();
+		} catch (ProcessFailedException $exception) {
+			// En cas d'échec de Composer Audit, retourner un message d'erreur
+			return $exception->getMessage();
+		}
+	}
 	private function isValidGitUrl($url)
 	{
 		// Expression régulière pour valider l'URL Git
