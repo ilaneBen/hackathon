4ddce0decd1e3a22d7c57e1f5f224fd894a3e498
BDMaxiTacos <HideSnake.Gaming@gmail.com>
2024-01-10 15:08:04 +0100

    signup in loginauthenticator for auto signin

--- src/Controller/Api/ApiUserController.php
+++ src/Controller/Api/ApiUserController.php
@@ -3,12 +3,7 @@
 namespace App\Controller\Api;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use App\Entity\User;
-use App\Security\LoginAuthenticator;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/', 'user_')]
@@ -21,44 +16,8 @@ class ApiUserController extends AbstractController
     }
 
     #[Route('/signup', name: 'signup', methods: ['POST'])]
-    public function signUp(
-        Request $request,
-        EntityManagerInterface $em,
-        LoginAuthenticator $loginAuthenticator,
-        UserPasswordHasherInterface $passwordHasher
-    ): Response {
-
-        $inputBag = $request->request;
-        $email = $inputBag->get('email');
-        $password = $inputBag->get('password');
-        $firstName = $inputBag->get('firstName');
-        $name = $inputBag->get('name');
-
-        if (!$email || !$password || !$firstName || !$name) {
-            return $this->json(['code' => 400, 'message' => "Veuillez remplir tous les champs."]);
-        }
-
-        $user = $em->getRepository(User::class)->findOneBy(['email' => $email]);
-        if ($user) {
-            return $this->json(['code' => 400, 'message' => "Un utilisateur avec cet email existe déjà."]);
-        }
-
-        $user = new User;
-        $hashedPassword = $passwordHasher->hashPassword(
-            $user,
-            $password
-        );
-
-        $user->setEmail($email);
-        $user->setPassword($hashedPassword);
-        $user->setFirstName($firstName);
-        $user->setName($name);
-
-        $em->persist($user);
-        $em->flush();
-
-        $loginAuthenticator->authenticate($request);
-
+    public function signUp(): Response 
+    {
         return $this->json(['code' => 200, 'message' => "User created and connected"]);
     }
 }
--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -2,9 +2,13 @@
 
 namespace App\Security;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
@@ -16,6 +20,16 @@ use Symfony\Component\Security\Http\SecurityRequestAttributes;
 
 class LoginAuthenticator extends AbstractAuthenticator
 {
+    public function __construct(
+        private EntityManagerInterface $em,
+        private LoginAuthenticator $loginAuthenticator,
+        private UserPasswordHasherInterface $passwordHasher,
+        private UserRepository $userRepository,
+    )
+    {
+        
+    }
+
     public function supports(Request $request): ?bool
     {
         return $request->headers->has('X-AUTH-TOKEN') || $request->request->get('csrf');
@@ -27,13 +41,40 @@ class LoginAuthenticator extends AbstractAuthenticator
         $inputBag = $request->request;
 
         $email = $inputBag->get('email');
+        $password = $inputBag->get('password');
+        $firstName = $inputBag->get('firstName');
+        $name = $inputBag->get('name');
         $csrfToken = $inputBag->get('csrf');
 
         $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $email);
 
+        if ($email && $password) {
+            $user = $this->userRepository->findOneBy(['email' => $email]);
+
+            if (!$user && $firstName && $name) {
+                $inputBag = $request->request;
+
+                $user = new User;
+                $hashedPassword = $this->passwordHasher->hashPassword(
+                    $user,
+                    $password
+                );
+
+                $user->setEmail($email);
+                $user->setPassword($hashedPassword);
+                $user->setFirstName($firstName);
+                $user->setName($name);
+
+                $this->em->persist($user);
+                $this->em->flush();
+            }
+        }else{
+            $email = $password = "";
+        }
+
         return new Passport(
             new UserBadge($email),
-            new PasswordCredentials($inputBag->get('password')),
+            new PasswordCredentials($password),
             [
                 new CsrfTokenBadge('signin', $csrfToken),
             ]
