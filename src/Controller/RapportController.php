f720ff8a2ab83096edab81bae2929670aa973fbf
rahdy <rahdy@code-rhapsodie.fr>
2024-01-10 22:21:59 +0100

    add rapportController

---
+++ src/Controller/RapportController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Rapport;
+use App\Form\RapportType;
+use App\Repository\RapportRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/rapport')]
+class RapportController extends AbstractController
+{
+
+
+    #[Route('/{id}', name: 'app_rapport_show', methods: ['GET'])]
+    public function show(Rapport $rapport): Response
+    {
+        return $this->render('rapport/show.html.twig', [
+            'rapport' => $rapport,
+        ]);
+    }
+
+
+    #[Route('/{id}', name: 'app_rapport_delete', methods: ['POST'])]
+    public function delete(Request $request, Rapport $rapport, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$rapport->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($rapport);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_rapport_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
