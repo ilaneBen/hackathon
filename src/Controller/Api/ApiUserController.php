574e8b666a7e485718a2df626d30fcf4b77132d8
BDMaxiTacos <HideSnake.Gaming@gmail.com>
2024-01-11 12:15:09 +0100

    fix hasher

--- src/Controller/Api/ApiUserController.php
+++ src/Controller/Api/ApiUserController.php
@@ -2,19 +2,18 @@
 
 namespace App\Controller\Api;
 
-use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/', 'user_')]
 class ApiUserController extends AbstractController
 {
     public function __construct(
-        private UserPasswordHasher $passwordHasher
+        private UserPasswordHasherInterface $passwordHasher
     ) {}
 
     #[Route('/signin', name: 'signin', methods: ['POST'])]
