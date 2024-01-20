19437b430e88b58b57ebfb6c54b378e8389a5dcf
rahdy <rahdy@code-rhapsodie.fr>
2024-01-20 01:28:46 +0100

    add service  and com

--- src/Service/PhpCsAnalysisService.php
+++ src/Service/PhpCsAnalysisService.php
@@ -6,13 +6,13 @@ use Symfony\Component\Process\Process;
 
 class PhpCsAnalysisService
 {
-	/**
-	 * Exécute l'analyse PHP CodeSniffer (PHP CS) sur le répertoire spécifié.
-	 *
-	 * @param string $directory Le répertoire sur lequel effectuer l'analyse PHP CS.
-	 *
-	 * @return Process L'objet Process Symfony représentant l'analyse PHP CS.
-	 */
+    /**
+     * Exécute l'analyse PHP CodeSniffer (PHP CS) sur le répertoire spécifié.
+     *
+     * @param string $directory le répertoire sur lequel effectuer l'analyse PHP CS
+     *
+     * @return process L'objet Process Symfony représentant l'analyse PHP CS
+     */
     public function runPhpCsAnalysis(string $directory): Process
     {
         $process = new Process(['../../vendor/bin/phpcs', '../../public/repoClone/src']);
