5eb8db11002d9ac4b01dcb5f363df14f8055da90
rahdy <rahdy@code-rhapsodie.fr>
2024-01-31 14:17:24 +0100

    rapport page

--- src/Model/JobViewModel.php
+++ src/Model/JobViewModel.php
@@ -36,17 +36,66 @@ class JobViewModel
 
         switch ($this->job->getName()) {
             case 'Composer Audit':
-                return [
-                    'Version' => $details['version'] ?? 'Non spécifié',
-                ];
+                $composerAuditResults = [];
 
-            case 'PHP STAN':
+                foreach ($details['result'] as $packageResult) {
+                    foreach ($packageResult as $packageName => $vulnerabilities) {
+                        $vulnerabilityDetails = [];
+
+                        foreach ($vulnerabilities as $vulnerability) {
+                            $vulnerabilityDetails[] = [
+                                'CVE' => $vulnerability['cve'] ?? 'Non spécifié',
+                                'Link' => $vulnerability['link'] ?? 'Non spécifié',
+                                'Title' => $vulnerability['title'] ?? 'Non spécifié',
+                                'Sources' => $vulnerability['sources'] ?? [],
+                                'Advisory ID' => $vulnerability['advisoryId'] ?? 'Non spécifié',
+                                'Reported At' => $vulnerability['reportedAt'] ?? 'Non spécifié',
+                            ];
+                        }
+
+                        $composerAuditResults[] = [
+                            'Package Name' => $packageName,
+                            'Vulnerabilities' => $vulnerabilityDetails,
+                        ];
+                    }
+                }
+
                 return [
+                    'Composer Audit Results' => $composerAuditResults,
                 ];
 
             case 'PHP Version':
                 return [
                     'Version minimale requise' => $details['result'][0] ?? 'Non spécifié',
+                ];
+            case 'PHP STAN':
+                $phpStanResults = [];
+
+                foreach ($details['result'] as $index => $result) {
+                    $files = [];
+
+                    foreach ($result as $filePath => $fileDetails) {
+                        if ('errors' !== $filePath && 'warnings' !== $filePath && 'fixable' !== $filePath) {
+                            $files[] = [
+                                'Chemin du fichier' => $filePath,
+                                'Erreurs' => $fileDetails['errors'] ?? 0,
+                                'Avertissements' => $fileDetails['warnings'] ?? 0,
+                                'Messages' => $fileDetails['messages'] ?? [],
+                            ];
+                        }
+                    }
+
+                    $phpStanResults[] = [
+                        'Index' => $index + 1,
+                        'Erreurs' => $result['errors'] ?? 0,
+                        'Avertissements' => $result['warnings'] ?? 0,
+                        'Fichiers' => $files,
+                        // Ajoutez d'autres éléments si nécessaire
+                    ];
+                }
+
+                return [
+                    'Résultats de PHP STAN' => $phpStanResults,
                 ];
 
             case 'PHP Cs':
