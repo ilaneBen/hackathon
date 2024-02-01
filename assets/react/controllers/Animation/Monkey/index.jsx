cd600253f372ac0851d3a08933d0df3cd824be72
admin <admin@VIL-S01-iMac-ITA-12.local>
2024-02-01 10:09:17 +0100

    monkey animation

--- assets/react/controllers/Animation/Monkey/index.jsx
+++ assets/react/controllers/Animation/Monkey/index.jsx
@@ -3,34 +3,46 @@ import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 export default function Monkey() {
-  const width = window.innerWidth,
-    height = window.innerHeight;
+  const width = 500,
+    height = 500;
 
   const scene = new THREE.Scene();
+  scene.background = null; // Fond transparent
 
   const light = new THREE.AmbientLight(0xffffff); // Lumière blanche
   scene.add(light);
 
   const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
-  camera.position.z = 5;
-  camera.position.x = 5;
+  camera.position.set(0, 0, 6);
 
-  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true }); // Fond transparent
   renderer.setSize(width, height);
   renderer.setAnimationLoop(animation);
+  renderer.shadowMap.enabled = true; // Ombres
 
   const container = useRef();
+  let monkey;
+  let pivot;
+
   useEffect(() => {
     container.current.appendChild(renderer.domElement);
 
-    // Charger le modèle glTF
     const loader = new GLTFLoader();
-    loader.load("animation/jiggly_pudding.glb", (gltf) => {
-      const monkey = gltf.scene;
-      scene.add(monkey);
+    loader.load("animation/monkey_chirurgie.glb", (gltf) => {
+      monkey = gltf.scene;
+      console.log(monkey);
+      const boundingBox = new THREE.Box3().setFromObject(monkey);
+      const center = boundingBox.getCenter(new THREE.Vector3());
+      monkey.position.sub(center);
+
+      monkey.map = THREE.LinearFilter; // Anti-aliasing
+
+      pivot = new THREE.Group();
+      pivot.add(monkey);
+
+      scene.add(pivot);
     });
 
-    // Nettoyer la scène et le rendu lors du démontage du composant
     return () => {
       scene.dispose();
       renderer.dispose();
@@ -38,7 +50,9 @@ export default function Monkey() {
   }, [renderer]);
 
   function animation(time) {
-    // Ajouter ici vos logiques d'animation si nécessaire
+    if (pivot) {
+      pivot.rotation.y += 0.01;
+    }
     renderer.render(scene, camera);
   }
 
