fc12938fd4a58ea8d7437d57310561e73b00e197
BDMaxiTacos <HideSnake.Gaming@gmail.com>
2024-02-06 10:18:37 +0100

    delete useless imports

--- src/Controller/Admin/AdminUserController.php
+++ src/Controller/Admin/AdminUserController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
